e2795e64750378266ee4c46fe8c86f43386d2ed1
Vishal Yadav <vishalyadav95692008@email.in>
2025-11-09 23:35:06 +0530

    frist commit

---
+++ ageprint.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    char star ='#';
+    printf("my age is %c\n", star);
+    return 0;
+}
---
+++ areaofsquare.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int side;
+    printf("enter side");
+    scanf("%d", &side);
+    printf("area of square :%d",side*side);
+    return 0;
+}
